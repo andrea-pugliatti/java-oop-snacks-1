c49348e99e84a214ddad80da745144900870edd5
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-06 19:32:50 +0100

    add corrections to first snack

--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -11,7 +11,36 @@ public class Studente {
         this.età = età;
     }
 
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getEtà() {
+        return this.età;
+    }
+
+    public void setEtà(int età) {
+        this.età = età;
+    }
+
     public String getNomeEsteso() {
-        return nome + " " + cognome + ", " + età + " anni";
+        return String.format("%s %s, %d", nome, cognome, età);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s, %d", nome, cognome, età);
     }
 }
