ef6866628891788a7622c36764513bec79056ba6
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-02-06 18:29:00 +0100

    add first snack solution

--- src/org/lessons/java/snacks/Main.java
+++ src/org/lessons/java/snacks/Main.java
@@ -2,6 +2,8 @@ package org.lessons.java.snacks;
 
 public class Main {
     public static void main(String[] args) {
+        Studente anselmo = new Studente("Anselmo", "Bitta", 24);
 
+        System.out.println(anselmo.getNomeEsteso());
     }
 }
--- src/org/lessons/java/snacks/Studente.java
+++ src/org/lessons/java/snacks/Studente.java
@@ -1,5 +1,17 @@
 package org.lessons.java.snacks;
 
 public class Studente {
+    private String nome;
+    private String cognome;
+    private int età;
 
+    public Studente(String nome, String cognome, int età) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.età = età;
+    }
+
+    public String getNomeEsteso() {
+        return nome + " " + cognome + ", " + età + " anni";
+    }
 }
